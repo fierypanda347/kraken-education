72ddc4fab322081bd112bf94de41315719f81726
Advait Variyar <advaitvariyar7@gmail.com>
2022-07-19 20:22:18 +0530

    🚀 add: Add Class Button
    Add Home Screen Button

--- src/renderer/pages/Home.jsx
+++ src/renderer/pages/Home.jsx
@@ -1,13 +1,27 @@
 import './Home.css'
+import { Link } from 'react-router-dom'
 
 export default function Home() {
   return (
     <div className="page" id="home">
       <div id="class-container">
+<<<<<<< Updated upstream
         <div id="class-container-header">
           <h1 id="class-container-title">Your Classes</h1>
           <button id="add-class-button">+&nbsp;Add Class</button>
         </div>
+=======
+<<<<<<< Updated upstream
+        <h1 id="class-container-title">Your Classes</h1>
+=======
+        <div id="class-container-header">
+          <h1 id="class-container-title">Your Classes</h1>
+            <Link to="/add-class">
+              <button id="add-class-button">+&nbsp;Add Class</button>
+            </Link>
+        </div>
+>>>>>>> Stashed changes
+>>>>>>> Stashed changes
       </div>
     </div>
   )
